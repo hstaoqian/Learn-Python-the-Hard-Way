bf38a25f37c22889f07c59af0612af94e1f32797
hstaoqian <hstaoqian@me.com>
2016-12-22 22:54:38 +0800

    complete ex1-ex10

---
+++ ex5.py
@@ -0,0 +1,14 @@
+my_name = "Tao Qian"
+my_age = 31
+my_height = 181 # cm
+my_weight = 83 # kg
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Black"
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
